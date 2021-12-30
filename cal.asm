997a6412d7ddfd2a59e8360129f54fc590046a83
mohmed30 <48033127+mohmed30@users.noreply.github.com>
2021-12-30 18:49:32 +0200

    Update cal.asm

--- cal.asm
+++ cal.asm
@@ -4,8 +4,8 @@ includelib \masm32\lib\user32.lib
 .data
 
 msg1:    db      "1-Add",0dh,0ah,"2-Multiply",0dh,0ah,"3-Subtract",0dh,0ah,"4-Divide", 0Dh,0Ah, '$'
-msg2:    db      0dh,0ah,"Enter First No : $"
-msg3:    db      0dh,0ah,"Enter Second No : $"
+msg2:    db      0dh,0ah,"Enter First Number : $"
+msg3:    db      0dh,0ah,"Enter Second Number : $"
 msg4:    db      0dh,0ah,"Choice Error $" 
 msg5:    db      0dh,0ah,"Result : $" 
 msg6:    db      0dh,0ah ,'thank you for using the calculator! press any key... ', 0Dh,0Ah, '$'
