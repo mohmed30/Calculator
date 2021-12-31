fda887817b340a221beaaf8559e16756bb4e4370
mohmed30 <48033127+mohmed30@users.noreply.github.com>
2021-12-31 18:30:18 +0200

    Update cal.asm

--- cal.asm
+++ cal.asm
@@ -3,7 +3,7 @@ includelib \masm32\lib\user32.lib
 
 .data
 
-msg1:    db      "1-Add",0dh,0ah,"2-Multiply",0dh,0ah,"3-Subtract",0dh,0ah,"4-Divide", 0Dh,0Ah, '$'
+msg1:    db      "1-Add",0dh,0ah,"2-Multiply",0dh,0ah,"3-Subtract",0dh,0ah,"4-Divide", 0Dh,0Ah, '$' 
 msg2:    db      0dh,0ah,"Enter First Number : $"
 msg3:    db      0dh,0ah,"Enter Second Number : $"
 msg4:    db      0dh,0ah,"Choice Error $" 
