025082f2eb9df78508d52fccb872343ad510bdcd
mohmed30 <48033127+mohmed30@users.noreply.github.com>
2021-12-31 22:36:32 +0200

    Update cal.asm

--- cal.asm
+++ cal.asm
@@ -1,45 +1,7 @@
-org 100h
 
 .data
 
-msg1:    db      "1-Add",0dh,0ah,"2-Multiply",0dh,0ah,"3-Subtract",0dh,0ah,"4-Divide", 0Dh,0Ah, '$' 
-msg2:    db      0dh,0ah,"Enter First Number : $"
-msg3:    db      0dh,0ah,"Enter Second Number : $"
-msg4:    db      0dh,0ah,"Choice Error $" 
-msg5:    db      0dh,0ah,"Result : $" 
-msg6:    db      0dh,0ah ,'thank you for using the calculator! press any key... ', 0Dh,0Ah, '$'
-
 
 .code
-start:  
-        mov ah, 9
-        mov dx, offset msg1 
-        int 21h
-        mov ah, 0                       
-        int 16h  
-        cmp al, 31h 
-        je Addtion
-        cmp al, 32h
-        je Multiply
-        cmp al, 33h
-        je Subtract
-        cmp al, 34h
-        je Divide
-        mov ah, 9
-        mov dx, offset msg4
-        int 21h
-        mov ah, 0
-        int 16h
-        jmp start
- Addtion:
- 
- 
- Multiply:
- 
- 
- Subtract:
- 
- 
- 
- Divide:
+       
  
